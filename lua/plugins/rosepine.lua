20d2625b781180ce6880137bc3fe49fcc3339e12
Brian Dawn <brian.t.dawn@gmail.com>
2024-11-23 10:53:16 -0600

    Disable italics.

--- lua/plugins/rosepine.lua
+++ lua/plugins/rosepine.lua
@@ -1,3 +1,14 @@
-return { "rose-pine/neovim", name = "rose-pine" }
+return { "rose-pine/neovim", name = "rose-pine", config = function()
+    local rose_pine = require("rose-pine")
+
+    rose_pine.setup({
+        styles = {
+            italic = false,
+            bold = true
+      }
+    })
+end
+}
+
 
 
